abee3b91cd8ae8c5d6b5357841dff4a155dff83a
peotr26 <67782057+peotr26@users.noreply.github.com>
2022-04-10 09:07:35 +0200

    Update dnf-boost.sh

--- dnf-boost.sh
+++ dnf-boost.sh
@@ -1,15 +1,15 @@
 #/bin/sh
 
-sudo cp /etc/dnf/dnf.conf /etc/dnf.dnf.conf.bak #Create a backup
+cp /etc/dnf/dnf.conf /etc/dnf.dnf.conf.bak #Create a backup
 
-sudo echo fastestmirror=True >> /etc/dnf/dnf.conf #Enable fast mirror
-sudo echo keepcache=True >> /etc/dnf/dnf.conf #Keep the cache
+echo fastestmirror=True >> /etc/dnf/dnf.conf #Enable fast mirror
+echo keepcache=True >> /etc/dnf/dnf.conf #Keep the cache
 
 #Enable parallel downloads
 while true; do
     read -p "Do you want parallel download to be activated? (Only if you have a good internet connection) [y/N]" yn
     case $yn in
-        [Yy]* ) sudo echo max_parallel_downloads=10 >> /etc/dnf/dnf.conf; break;;
+        [Yy]* ) echo max_parallel_downloads=10 >> /etc/dnf/dnf.conf; break;;
         [Nn]* ) break;;
         * ) break;;
     esac
@@ -19,7 +19,7 @@ done
 while true; do
     read -p "Do you want Yes to be the default? (Not recommanded) [y/N]" yn
     case $yn in
-        [Yy]* ) sudo echo defaultyes=True >> /etc/dnf/dnf.conf; break;;
+        [Yy]* ) echo defaultyes=True >> /etc/dnf/dnf.conf; break;;
         [Nn]* ) break;;
         * ) break;;
     esac
